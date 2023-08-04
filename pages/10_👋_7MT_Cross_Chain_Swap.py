cf760b09f5aa4ed43e5b367fdcd8fc509353fb92
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-03 14:07:12 -1000

    Update 10_👋_7MT_Cross_Chain_Swap.py

--- pages/10_👋_7MT_Cross_Chain_Swap.py
+++ pages/10_👋_7MT_Cross_Chain_Swap.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="7MT CrossChain Swap",
+    page_title="ironMirror CrossChain SwapService",
     page_icon="👋",
 )
 
-st.write("# Welcome to 7MT Cross Chain Swap Information Hub! 👋")
+st.write("# Welcome to ironMirror Cross Chain Swap Information Hub! 👋")
 
 st.markdown(
     """
     ### Want to learn more?
-    - Check out [7MT-Cross-Chain-Swap Demo](https://7mt-swap-x-cleancoindev.vercel.app/)
+    - Check out [ironMirror-Cross-Chain-Swap Demo](http://)
 
     ### See more complex demos
     - Use a neural net to [analyze the Udacity Self-driving Car Image
