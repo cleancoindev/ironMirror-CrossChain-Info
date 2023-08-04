0a4998f8085417c1b1f9ac86c5af119354976900
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-03 14:11:34 -1000

    Update and rename 12_💰_7MTFUND_Claim_App.py to 3_💰_7MTFUND_Claim_App.py

---
+++ pages/3_💰_7MTFUND_Claim_App.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ironMirror-ART Claim",
+    page_icon="💰",
+)
+
+st.write("# Welcome to ironMirror Claim App Information Hub Page! 💰")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - Check out https://iron-mirror-v1.vercel.app/
+
+"""
+)
