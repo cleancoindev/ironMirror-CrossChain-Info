01fa8a64e09ba7a7bc66eb094ca3df6db5f875fb
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-08-03 14:09:13 -1000

    Update and rename 11_👋_7MT_Market_Zero.py to 2_👋_7MT_Market_Zero.py

---
+++ pages/2_👋_7MT_Market_Zero.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="7MT Marketplace Zero",
+    page_icon="👋",
+)
+
+st.write("# Welcome to ironMirror-ART Marketplace Zero! 👋")
+
+st.markdown(
+    """
+    ### 
+    - Check out [ironMirror-ART Market Zero!]()
+
+
+"""
+)
