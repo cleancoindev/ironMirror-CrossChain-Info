df6d1e3b892f238388324a6811c6c0d89203be32
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-10-23 09:45:33 -1000

    Update 3_💰_ironMirror_Claim_App.py

--- pages/3_💰_ironMirror_Claim_App.py
+++ pages/3_💰_ironMirror_Claim_App.py
@@ -9,8 +9,14 @@ st.write("# Welcome to ironMirror Claim App Information Hub Page! 💰")
 
 st.markdown(
     """
-    ### Want to learn more?
-    - Check out https://iron-mirror-v1.vercel.app/
-
+    ### Want some irMirror Art tokens?
+    - Mint below with your IRON!
+<iframe
+    src="https://embed.ipfscdn.io/ipfs/bafybeigdie2yyiazou7grjowoevmuip6akk33nqb55vrpezqdwfssrxyfy/erc20.html?contract=0xc1a0961AD9bf1609721856F1Da2542A2f632471C&chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fpolygon%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=a1fbe50aa0daf1885a198855f039d54b&theme=dark&primaryColor=purple"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+></iframe>
 """
 )
