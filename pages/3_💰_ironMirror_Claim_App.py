e1d3d5d9912b3786e13cd0f16998aa9bc8b2f888
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-10-23 09:48:01 -1000

    Update 3_💰_ironMirror_Claim_App.py

--- pages/3_💰_ironMirror_Claim_App.py
+++ pages/3_💰_ironMirror_Claim_App.py
@@ -1,16 +1,18 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="ironMirror-ART Claim",
+    page_title="Iron Mirror Art Token-Minting Page - Would you like to mint IRON MIRROR Art tokens with your IRON?",
     page_icon="💰",
 )
 
-st.write("# Welcome to ironMirror Claim App Information Hub Page! 💰")
-
-st.markdown(
+# bootstrap 4 collapse example
+components.html(
     """
-    ### Want some irMirror Art tokens?
-    - Mint below with your IRON!
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <iframe
     src="https://embed.ipfscdn.io/ipfs/bafybeigdie2yyiazou7grjowoevmuip6akk33nqb55vrpezqdwfssrxyfy/erc20.html?contract=0xc1a0961AD9bf1609721856F1Da2542A2f632471C&chain=%7B%22name%22%3A%22Polygon+Mainnet%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fpolygon.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22matic%22%2C%22chainId%22%3A137%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22polygon%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fpolygon%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=a1fbe50aa0daf1885a198855f039d54b&theme=dark&primaryColor=purple"
     width="600px"
@@ -18,5 +20,7 @@ st.markdown(
     style="max-width:100%;"
     frameborder="0"
 ></iframe>
-"""
+    """,
+    height=600,
+    width=600,
 )
